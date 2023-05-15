d51fc5f7426a116225988f9318e8717d23162617
yoshiko <ttkmysk021@gmail.com>
2023-05-15 21:11:51 +0900

    first creat

---
+++ script.js
@@ -0,0 +1,26 @@
+  // 1
+    // elseを使用するときは、７の値がtrueかfalseどちらなのかで処理をしなくてはならないのか考えながら作りました。
+
+    // 2
+    // 実装したときはデベロッパーを確認し、
+    // エラーになってないか確認しながら作りました。
+    
+    // 3
+    var variableNumber = 7 ;
+    if(variableNumber){
+      console.log("trueの処理");
+      console.log(variableNumber);
+    }else{
+      console.log("falseの処理");
+      console.log(variableNumber);
+    }
+
+    // 4
+    var variableText = "text" ;
+    if(variableText){
+      console.log("trueの処理");
+      console.log(variableText);
+    }else{
+      console.log("falseの処理");
+      console.log(variableText);
+    }
